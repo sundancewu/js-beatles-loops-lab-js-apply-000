bebb308fac378b901107108a7059f2a3029ddedc
sundancewu <sundancewu@gmail.com>
2018-08-06 06:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var phrases = [];
+  var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    phrases.push(musicians[i] + " plays " + instruments[i]);
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
   }
-  return phrases;
+  return array;
 }
- function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
-  var i = 0
-  while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
-    i++;
+ function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
   }
-  return newFacts;
+  return results;
 }
  function iLoveTheBeatles(num) {
-  var phrases = [];
+  var array = [];
   do {
-    phrases.push("I love the Beatles!");
-    num++
+    array.push("I love the Beatles!")
+    num++;
   } while (num < 15);
-  return phrases;
-} 
+  return array;
+}
